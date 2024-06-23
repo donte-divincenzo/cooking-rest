a3d906873de9b70d18200f22b15b1126869441cc
anton-gandonio <for.site.joins@gmail.com>
2024-06-23 08:19:46 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slides');
+
+    function showSlide(index) {
+        slides.forEach(slide => slide.classList.remove('active-slide'));
+        slides[index].classList.add('active-slide');
+    }
+
+    function changeSlide(direction) {
+        currentSlide += direction;
+        if (currentSlide < 0) {
+            currentSlide = slides.length - 1;
+        } else if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+        showSlide(currentSlide);
+    }
+
+    // Initial slide
+    showSlide(currentSlide);
